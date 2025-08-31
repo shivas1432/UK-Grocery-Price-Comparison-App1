85eb584a3825949b15ed408bb5f92f1d4992646f
Kanugula Shivashanker <145932982+shivas1432@users.noreply.github.com>
2025-08-31 20:42:13 +0100

    Update sw.js
    updated sw.js

--- public/sw.js
+++ public/sw.js
@@ -16,7 +16,7 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Fetch event - serve from cache when offline
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
